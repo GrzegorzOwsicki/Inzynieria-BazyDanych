3476d6f47b113e458eb70feb77986aaa9d27fd26
GrzegorzOwsicki <117662448+GrzegorzOwsicki@users.noreply.github.com>
2024-05-16 14:14:46 +0200

    Update settings.py

--- IOBD_Back/IOBD_Back/settings.py
+++ IOBD_Back/IOBD_Back/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-4obc6hrkdb+n@4@(2%j(pa7+ucnebyes)!yyf9ncke1-hl#9aq'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
